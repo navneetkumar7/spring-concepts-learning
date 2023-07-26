8a6a21c6e0aa19610983a609a46f8fee2d389141
navneetkumar7 <navneetthakur@Navneets-MacBook-Air.local>
2023-07-26 22:27:45 +0530

    step 2 spring security with user name password from property file

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This project contains spring features implementation.
 ### Project modules:
 
 1. spring-security-concepts
+    ##### Links:
+       1. https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html#appendix.application-properties.security
--- spring-security-concepts/src/main/java/com/nt/springconcepts/security/controller/SecurityController.java
+++ spring-security-concepts/src/main/java/com/nt/springconcepts/security/controller/SecurityController.java
@@ -3,10 +3,10 @@ package com.nt.springconcepts.security.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 public class SecurityController {
-    @GetMapping("/login")
+    @GetMapping("/users/login")
     public String login(){
-        return "logged in without security";
+        return "logged in with default password set";
     }
 }
